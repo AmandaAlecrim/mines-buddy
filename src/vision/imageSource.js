762ffac372efe71bea8fb31da0cda7885e4fab05
Amanda Demétrio Alecrim <42013152+AmandaAlecrim@users.noreply.github.com>
2026-05-02 18:14:41 -0300

    chore: remove módulos vazios não utilizados
    Apaga src/vision/ inteiro e src/ui/render.js, todos com 0 bytes
    e sem importações em nenhum lugar do projeto.

--- src/vision/imageSource.js
+++
@@ -1,8 +0,0 @@
-/** @param {File} file @returns {Promise<ImageBitmap>} */
-export async function fileToImageBitmap(file) {
-  if (!(file instanceof File)) {
-    throw new Error("Esperado File para conversão em ImageBitmap.");
-  }
-  return await createImageBitmap(file);
-}
-
--- src/vision/index.js
+++
@@ -1,3 +0,0 @@
-export { fileToImageBitmap } from "./imageSource.js";
-export { ocrMinesweeperBoard } from "./ocr.js";
-
